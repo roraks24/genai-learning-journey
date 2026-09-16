28b52e4f841bf30c693ec7662689af5c6ecf6f03
roraks24 <rsaini2407@gmail.com>
2026-09-16 11:12:21 +0530

    Day 28: Conditional routing and stateful workflow

--- week5_langGraph/learning_graph/conditional_edges.py
+++ week5_langGraph/learning_graph/conditional_edges.py
@@ -4,6 +4,7 @@ from langgraph.graph import StateGraph, START, END
 class GraphState(TypedDict):
     a : int
     result : str
+    count : int
     formatted_result : str
 
 def check_number(state: GraphState):
@@ -25,6 +26,12 @@ def even_number(state: GraphState):
 def odd_number(state: GraphState):
     return {"result": "a is odd"}
 
+def update_count(state: GraphState):
+    count = state["count"]
+    count += 1
+
+    return {"count" : count} 
+
     
 def format_result(state: GraphState):
     formatted_result = state['result']
@@ -37,6 +44,7 @@ builder = StateGraph(GraphState)
 builder.add_node("check_number", check_number)
 builder.add_node("even_number", even_number)
 builder.add_node("odd_number", odd_number)
+builder.add_node("update_count", update_count)
 builder.add_node("format_result", format_result)
 
 builder.add_edge(START, "check_number")
@@ -44,16 +52,16 @@ builder.add_edge(START, "check_number")
 builder.add_conditional_edges(
     "check_number", route_number
 )
-
-builder.add_edge("even_number", "format_result")
-builder.add_edge("odd_number" ,"format_result")
-
+builder.add_edge("even_number", "update_count")
+builder.add_edge("odd_number" ,"update_count")
+builder.add_edge("update_count", "format_result")
 builder.add_edge("format_result", END)
 
 graph = builder.compile()
 
 result = graph.invoke({
-    "a" : 69
+    "a" : 69,
+    "count" : 0
 })
 
 print(result)
